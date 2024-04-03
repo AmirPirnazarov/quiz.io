8705c7770d5e24c5e008dcfe2ed77331cf2acf45
Amir <106487104+AmirPirnazarov@users.noreply.github.com>
2024-04-03 16:00:07 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -50,6 +50,11 @@ close.addEventListener("click", () => {
     window.close();
 })
 close_result.addEventListener("click", () => { 
+    let date = {
+        name = Amir,
+        asda = 454,
+    }
+    tg.sendDate(JSON.stringify(date))
     tg.close();
     window.close();
 })
